e3bc178eabea4f950ab93848c9643fffa151de66
Douglas Vasconcelos <dssv@cesar.org.br>
2016-11-30 09:42:18 -0300

    Storage methods changed to 16 bits.
    Functions of read and write adapted to read and write with 16-bit data.

--- src/hal/storage/storage_avr.cpp
+++ src/hal/storage/storage_avr.cpp
@@ -10,11 +10,11 @@
 #include "storage.h"
 
 // Address where each data is stored at the end of EEPROM
-#define ADDR_UUID		0
-#define ADDR_TOKEN		41
-#define ADDR_MAC		81
-#define ADDR_SIZE_CONFIG	92
-#define ADDR_CONFIG		93
+#define ADDR_UUID		41
+#define ADDR_TOKEN		81
+#define ADDR_MAC		91
+#define ADDR_SIZE_CONFIG	93
+#define ADDR_CONFIG		94
 
 // Identifier of data type to be stored
 #define DATA_UUID		0
@@ -22,63 +22,61 @@
 #define DATA_MAC		2
 #define DATA_CONFIG		3
 
-int hal_storage_read(uint16_t addr, uint8_t *value, uint16_t len)
+int hal_storage_read(uint16_t addr, uint16_t *value, uint16_t len)
 {
-	uint8_t i;
+	uint16_t i;
 
 	/* E2END represents the last EEPROM address */
 	for (i = 0; i < len; i++) {
 		if((addr + i) > (E2END + 1))
 			break;
 
-		value[i] = eeprom_read_byte((const uint8_t*) addr + i);
+		value[i] = eeprom_read_word((const uint16_t*) addr + i);
 	}
 
 	return i;
 }
 
-int hal_storage_write(uint16_t addr, const uint8_t *value, uint16_t len)
+int hal_storage_write(uint16_t addr, const uint16_t *value, uint16_t len)
 {
-	uint8_t i;
+	uint16_t i;
 
 	/* E2END represents the last EEPROM address */
 	for (i = 0; i < len; i++) {
 		if ((addr + i) > (E2END + 1))
 			break;
 
-		eeprom_write_byte((uint8_t*) addr + i, value[i]);
+		eeprom_write_word((uint16_t*) addr + i, value[i]);
 	}
 
 	return i;
 }
 
-int hal_storage_write_end(void *value, int8_t len, uint8_t data)
+int hal_storage_write_end(void *value, int16_t len, uint8_t type)
 {
 	/* Position where the data will be stored */
 	uint16_t dst;
 
-	/* Pointer to the size of data, used as parameter */
-	const uint8_t *lenght = (const uint8_t*) &len;
-
 	/* Calculate different addresses to store the
 	 * value according to the parameter passed.
 	 */
-	switch (data) {
+	switch (type) {
 	case DATA_UUID:
-		dst = E2END - len - ADDR_UUID;
+		dst = E2END - ADDR_UUID;
 		break;
 	case DATA_TOKEN:
-		dst = E2END - len - ADDR_TOKEN;
+		dst = E2END - ADDR_TOKEN;
 		break;
 	case DATA_MAC:
-		dst = E2END - len - ADDR_MAC;
+		dst = E2END - ADDR_MAC;
 		break;
 	case DATA_CONFIG:
 		dst = E2END - len - ADDR_CONFIG;
 		/* Store the size of the config, 2 bytes, to know
 		 * where it end in the EEPROM.
 		 */
-		hal_storage_write(E2END - ADDR_SIZE_CONFIG, lenght, 2);
+		hal_storage_write(E2END - ADDR_SIZE_CONFIG,
+						(const uint16_t*)dst, 1);
 		break;
 	default:
 		return -1;
@@ -90,33 +88,32 @@ int hal_storage_write_end(void *value, int8_t len, uint8_t data)
 	return len;
 }
 
-int hal_storage_read_end(void *value, int8_t len, uint8_t data)
+int hal_storage_read_end(void *value, int16_t len, uint8_t type)
 {
 	/* Position where the data will be stored */
 	uint16_t dst;
 
-	/* Pointer to the size of data, used as parameter */
-	uint8_t *lenght = (uint8_t*) &len;
+	uint16_t *config_size = 0;
 
 	/* Calculate different addresses to read the
 	 * value according to the parameter passed.
 	 */
-	switch (data) {
+	switch (type) {
 	case DATA_UUID:
-		dst = E2END - len - ADDR_UUID;
+		dst = E2END - ADDR_UUID;
 		break;
 	case DATA_TOKEN:
-		dst = E2END - len - ADDR_TOKEN;
+		dst = E2END - ADDR_TOKEN;
 		break;
 	case DATA_MAC:
-		dst = E2END - len - ADDR_MAC;
+		dst = E2END - ADDR_MAC;
 		break;
 	case DATA_CONFIG:
 		/* Read the size of the config, 2 bytes, to know
 		 * where it end in the EEPROM.
 		 */
-		hal_storage_read(E2END - ADDR_SIZE_CONFIG, lenght, 2);
-		dst = E2END - len - ADDR_CONFIG;
+		hal_storage_read(E2END - ADDR_SIZE_CONFIG, config_size, 1);
+		dst = E2END - (uint16_t)config_size - ADDR_CONFIG;
 		break;
 	default:
 		return -1;
