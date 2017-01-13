93a49297658d2c7239c9c232e016d9ab28ebeb9c
Marcela Oliveira <mpo@cin.ufpe.br>
2017-01-13 09:13:56 -0300

    add functionality to persist private and public keys on EEPROM define IDs for private and public keys at storage.h and set private (32) and public(64) size at storage_avr private and public keys are been storaged on EEPROM after schema_flag

--- src/hal/storage/storage_avr.cpp
+++ src/hal/storage/storage_avr.cpp
@@ -15,6 +15,8 @@
 #define TOKEN_SIZE		40
 #define MAC_SIZE		8
 #define SCHEMA_FLAG_SIZE	1
+#define PRIVATE_KEY_SIZE	32
+#define PUBLIC_KEY_SIZE		64
 
 #define CONFIG_SIZE 		sizeof(uint16_t)
 
@@ -24,7 +26,9 @@
 #define ADDR_TOKEN		(ADDR_UUID - TOKEN_SIZE)
 #define ADDR_MAC		(ADDR_TOKEN - MAC_SIZE)
 #define ADDR_SCHEMA_FLAG	(ADDR_MAC - SCHEMA_FLAG_SIZE)
-#define ADDR_OFFSET_CONFIG	(ADDR_SCHEMA_FLAG - CONFIG_SIZE)
+#define ADDR_PRIVATE_KEY	(ADDR_SCHEMA_FLAG - PRIVATE_KEY_SIZE)
+#define ADDR_PUBLIC_KEY		(ADDR_PRIVATE_KEY - PUBLIC_KEY_SIZE)
+#define ADDR_OFFSET_CONFIG	(ADDR_PUBLIC_KEY - CONFIG_SIZE)
 
 #define EEPROM_SIZE_FREE	ADDR_OFFSET_CONFIG
 
@@ -84,30 +88,49 @@ ssize_t hal_storage_write_end(uint8_t id, void *value, size_t len)
 	 */
 	switch (id) {
 	case HAL_STORAGE_ID_UUID:
-		if(len != UUID_SIZE)
+		if (len != UUID_SIZE)
 			return -EINVAL;
 
 		dst = ADDR_UUID;
 		break;
+
 	case HAL_STORAGE_ID_TOKEN:
-		if(len != TOKEN_SIZE)
+		if (len != TOKEN_SIZE)
 			return -EINVAL;
 
 		dst = ADDR_TOKEN;
 		break;
+
 	case HAL_STORAGE_ID_MAC:
-		if(len != MAC_SIZE)
+		if (len != MAC_SIZE)
 			return -EINVAL;
 
 		dst = ADDR_MAC;
 		break;
+
 	case HAL_STORAGE_ID_SCHEMA_FLAG:
 		if (len != SCHEMA_FLAG_SIZE)
 			return -EINVAL;
+
 		dst = ADDR_SCHEMA_FLAG;
 		break;
+
+	case HAL_STORAGE_ID_PRIVATE_KEY:
+		if (len != PRIVATE_KEY_SIZE)
+			return -EINVAL;
+
+		dst = ADDR_PRIVATE_KEY;
+		break;
+
+	case HAL_STORAGE_ID_PUBLIC_KEY:
+		if (len != PUBLIC_KEY_SIZE)
+			return -EINVAL;
+
+		dst = ADDR_PUBLIC_KEY;
+		break;
+
 	case HAL_STORAGE_ID_CONFIG:
-		if(len > EEPROM_SIZE_FREE)
+		if (len > EEPROM_SIZE_FREE)
 			return -EINVAL;
 
 		/*
@@ -119,6 +142,7 @@ ssize_t hal_storage_write_end(uint8_t id, void *value, size_t len)
 		/* Compute config address offset at eeprom */
 		dst = ADDR_OFFSET_CONFIG - len;
 		break;
+
 	default:
 		return -EINVAL;
 	}
@@ -168,7 +192,6 @@ ssize_t hal_storage_read_end(uint8_t id, void *value, size_t len)
 		break;
 
 	case HAL_STORAGE_ID_CONFIG:
-
 		/*
 		 * Read the size of the config,
 		 * 2 bytes, to know where it end in the EEPROM.
